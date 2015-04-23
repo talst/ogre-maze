687d069a922f2e1a6883c054b0164b648ac96c26
Tal Steiner <tal.stx@gmail.com>
2015-04-23 17:26:04 +0300

    indentation

--- src/main/scala/ogre/maze/MazeDef.scala
+++ src/main/scala/ogre/maze/MazeDef.scala
@@ -1,46 +1,46 @@
-    package ogre.maze
+package ogre.maze
 
-    trait MazeDef {
+trait MazeDef {
 
-      case class Position(row: Int, col:Int) {
-        def dRow(d: Int) = copy(row = row + d)
-        def dCol(d: Int) = copy(col = col + d)
-      }
+  case class Position(row: Int, col:Int) {
+    def dRow(d: Int) = copy(row = row + d)
+    def dCol(d: Int) = copy(col = col + d)
+  }
 
-      val startPos1: Position
-      val startPos2: Position
-      val startPos3: Position
-      val startPos4: Position
-      val goal: Position
+  val startPos1: Position
+  val startPos2: Position
+  val startPos3: Position
+  val startPos4: Position
+  val goal: Position
 
-      type Terrain = Position => Boolean
+  type Terrain = Position => Boolean
 
-      val terrain: Terrain
+  val terrain: Terrain
 
-      sealed abstract class Move
-      case object Left extends Move
-      case object Right extends Move
-      case object Up extends Move
-      case object Down extends Move
+  sealed abstract class Move
+  case object Left extends Move
+  case object Right extends Move
+  case object Up extends Move
+  case object Down extends Move
 
-      def startOgre: Ogre = Ogre(startPos1, startPos2, startPos3, startPos4)
+  def startOgre: Ogre = Ogre(startPos1, startPos2, startPos3, startPos4)
 
-      case class Ogre(pos1: Position, pos2: Position, pos3: Position, pos4: Position) {
-        def dRow(d: Int) = copy(pos1.dRow(d), pos2.dRow(d), pos3.dRow(d), pos4.dRow(d))
-        def dCol(d: Int) = copy(pos1.dCol(d), pos2.dCol(d), pos3.dCol(d), pos4.dCol(d))
+  case class Ogre(pos1: Position, pos2: Position, pos3: Position, pos4: Position) {
+    def dRow(d: Int) = copy(pos1.dRow(d), pos2.dRow(d), pos3.dRow(d), pos4.dRow(d))
+    def dCol(d: Int) = copy(pos1.dCol(d), pos2.dCol(d), pos3.dCol(d), pos4.dCol(d))
 
-        def left = dCol(-1)
-        def right = dCol(1)
-        def up = dRow(-1)
-        def down = dRow(1)
+    def left = dCol(-1)
+    def right = dCol(1)
+    def up = dRow(-1)
+    def down = dRow(1)
 
-        def neighbors: List[(Ogre, Move)] = List((left, Left), (right, Right), (up, Up), (down, Down))
-        def legalNeighbors : List[(Ogre, Move)] = for {
-          (ogre, move) <- neighbors
-          if ogre.isLegal
-        } yield (ogre, move)
+    def neighbors: List[(Ogre, Move)] = List((left, Left), (right, Right), (up, Up), (down, Down))
+    def legalNeighbors : List[(Ogre, Move)] = for {
+      (ogre, move) <- neighbors
+      if ogre.isLegal
+    } yield (ogre, move)
 
-        def isLegal: Boolean = terrain(pos1) && terrain(pos2) && terrain(pos3) && terrain(pos4)
-      }
-    }
+    def isLegal: Boolean = terrain(pos1) && terrain(pos2) && terrain(pos3) && terrain(pos4)
+  }
+}
 
--- src/main/scala/ogre/maze/StringParserTerrain.scala
+++ src/main/scala/ogre/maze/StringParserTerrain.scala
@@ -1,55 +1,55 @@
-    package ogre.maze
+package ogre.maze
 
-    import scala.util.Try
+import scala.util.Try
 
-    trait StringParserTerrain extends MazeDef {
+trait StringParserTerrain extends MazeDef {
 
-      val level: String
+  val level: String
 
-      def terrainFunction(levelVector: Vector[Vector[Char]]): Position => Boolean =
-        (position: Position) => Try(levelVector(position.row)(position.col) != 'O').getOrElse(false)
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Position => Boolean =
+    (position: Position) => Try(levelVector(position.row)(position.col) != 'O').getOrElse(false)
 
-      def findChar(c: Char, levelVector: Vector[Vector[Char]]): Position = {
-        (for {
-          row <- levelVector.toStream
-          col <- row.toStream
-          if col == c
-        } yield Position(levelVector.indexOf(row), row.indexOf(col))).head
-      }
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Position = {
+    (for {
+      row <- levelVector.toStream
+      col <- row.toStream
+      if col == c
+    } yield Position(levelVector.indexOf(row), row.indexOf(col))).head
+  }
 
-      lazy val vector: Vector[Vector[Char]] = Vector(level.split("\n").map(str => Vector(str: _*)): _*)
+  lazy val vector: Vector[Vector[Char]] = Vector(level.split("\n").map(str => Vector(str: _*)): _*)
 
-      lazy val terrain: Terrain = terrainFunction(vector)
-      lazy val startPos1: Position = findChar('@', vector)
-      lazy val startPos2: Position = startPos1.dCol(1)
-      lazy val startPos3: Position = startPos1.dRow(1)
-      lazy val startPos4: Position = startPos1.dRow(1).dCol(1)
-      lazy val goal: Position = findChar('$', vector)
+  lazy val terrain: Terrain = terrainFunction(vector)
+  lazy val startPos1: Position = findChar('@', vector)
+  lazy val startPos2: Position = startPos1.dCol(1)
+  lazy val startPos3: Position = startPos1.dRow(1)
+  lazy val startPos4: Position = startPos1.dRow(1).dCol(1)
+  lazy val goal: Position = findChar('$', vector)
 
 
-      def pathToString(moves: List[Move], startOgre: Ogre, vector: Vector[Vector[Char]]): String = {
-        def updatePos(position: Position, vector: Vector[Vector[Char]]) = vector.updated(position.row, vector(position.row).updated(position.col, '&'))
+  def pathToString(moves: List[Move], startOgre: Ogre, vector: Vector[Vector[Char]]): String = {
+    def updatePos(position: Position, vector: Vector[Vector[Char]]) = vector.updated(position.row, vector(position.row).updated(position.col, '&'))
 
-        def updatedVector(ogre: Ogre, vector: Vector[Vector[Char]]): Vector[Vector[Char]] = {
-          val pos1Updated = updatePos(ogre.pos1, vector)
-          val pos2Updated = updatePos(ogre.pos2, pos1Updated)
-          val pos3Updated = updatePos(ogre.pos3, pos2Updated)
-          updatePos(ogre.pos4, pos3Updated)
-        }
-        def innerRec(moves: List[Move], ogre: Ogre, acc: Vector[Vector[Char]]): Vector[Vector[Char]] = moves match {
-          case List() => acc
-          case head :: tail => head match {
-            case Left => innerRec(tail, ogre.left, updatedVector(ogre.left, acc))
-            case Right => innerRec(tail, ogre.right, updatedVector(ogre.right, acc))
-            case Up => innerRec(tail, ogre.up, updatedVector(ogre.up, acc))
-            case Down => innerRec(tail, ogre.down, updatedVector(ogre.down, acc))
-          }
-        }
-        innerRec(moves, startOgre, updatedVector(startOgre, vector)).map(_.mkString("")).mkString("\n")
-      }
-
-      def solutionString(solution: List[Move], startOgre: Ogre, vector: Vector[Vector[Char]]): String = solution match {
-        case List() => "No path"
-        case head :: tail => pathToString(solution, startOgre, vector)
+    def updatedVector(ogre: Ogre, vector: Vector[Vector[Char]]): Vector[Vector[Char]] = {
+      val pos1Updated = updatePos(ogre.pos1, vector)
+      val pos2Updated = updatePos(ogre.pos2, pos1Updated)
+      val pos3Updated = updatePos(ogre.pos3, pos2Updated)
+      updatePos(ogre.pos4, pos3Updated)
+    }
+    def innerRec(moves: List[Move], ogre: Ogre, acc: Vector[Vector[Char]]): Vector[Vector[Char]] = moves match {
+      case List() => acc
+      case head :: tail => head match {
+        case Left => innerRec(tail, ogre.left, updatedVector(ogre.left, acc))
+        case Right => innerRec(tail, ogre.right, updatedVector(ogre.right, acc))
+        case Up => innerRec(tail, ogre.up, updatedVector(ogre.up, acc))
+        case Down => innerRec(tail, ogre.down, updatedVector(ogre.down, acc))
       }
     }
+    innerRec(moves, startOgre, updatedVector(startOgre, vector)).map(_.mkString("")).mkString("\n")
+  }
+
+  def solutionString(solution: List[Move], startOgre: Ogre, vector: Vector[Vector[Char]]): String = solution match {
+    case List() => "No path"
+    case head :: tail => pathToString(solution, startOgre, vector)
+  }
+}
